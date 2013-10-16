30101ca53a226a63877604b9d78ff98339768786
tothsa@gmail.com <tothsa@gmail.com@22f1fdcd-2ddb-f10a-0bf6-981d8240d77d>
2013-10-16 21:40:07 +0000

    Symmetry analysis READY! Go into testing phase!

--- branches/symdev/m_files/sw_version.m
+++ branches/symdev/m_files/sw_version.m
@@ -3,55 +3,60 @@ function varargout = sw_version()
 %
 
 % read file header from sw.m file
-fid = fopen([sw_rootdir 'm_files' filesep '@sw' filesep 'sw.m']);
+%fid = fopen([sw_rootdir 'm_files' filesep '@sw' filesep 'sw.m']);
+fid = fopen('sw.m');
 
 % first line
 fgets(fid);
 % skip comments
-fLine = fgets(fid);
-while strcmp(fLine(1),'%')
-    fLine = fgets(fid);
+fLine = strtrim(fgets(fid));
+while numel(fLine)>0 && strcmp(fLine(1),'%')
+    fLine = strtrim(fgets(fid));
 end
 % skip empty lines
-while ~strcmp(fLine(1),'%')
-    fLine = fgets(fid);
+while numel(fLine)==0 || ~strcmp(fLine(1),'%')
+    fLine = strtrim(fgets(fid));
 end
 % read version information
 verLine = {fLine};
-while strcmp(verLine{end}(1),'%')
-    verLine{end+1} = fgets(fid);
+while numel(verLine{end})>0 && strcmp(verLine{end}(1),'%')
+    verLine{end+1} = strtrim(fgets(fid)); %#ok<*AGROW>
 end
+verLine = verLine(1:end-1);
 fclose(fid);
 
-
-
-
-testStr = '% $Author: Sandor Toth$ ($Contact: sandor.toth@psi.ch$)';
-
-while numel(testStr) > 0
-    [~, testStr] = strtok(testStr,'$');
-    [partStr, testStr] = strtok(testStr,'$');
-    partStr
+% read strings enclosed with $ signs
+partStr = {};
+for ii = 1:numel(verLine)
+    verSel = verLine{ii};
+    while sum(verSel=='$') > 1
+        [~, verSel] = strtok(verSel,'$'); %#ok<*STTOK>
+        [partStr{end+1}, verSel] = strtok(verSel,'$');
+    end
+    
 end
 
+nField = numel(partStr);
+fieldName = cell(1,nField);
+fieldVal = cell(1,nField);
 
+verStruct = struct;
 
-build   = 'SpinW release 2.0beta';
-name    = 'SpinW';
-version = 2.0;
+% extract values and save them into a structure
+for ii = 1:nField
+    [fieldName{ii}, fieldVal{ii}] = strtok(partStr{ii},':');
+    fieldVal{ii} = strtrim(fieldVal{ii}(2:end));
+    verStruct.(fieldName{ii}) = fieldVal{ii};
+end
 
-build_data = '15.10.2013';
-contact    = 'sandor.toth@psi.ch';
-licence    = 'GNU GENERAL PUBLIC LICENSE';
+if nField == 0
+    varargout{1} = [];
+end
 
-if nargout ~= 1
-    disp(build)
+if nargout < 1
+    disp([verStruct.Name verStruct.Version ' (rev ' num2str(verStruct.Revision) ')']);
 else
-    varargout{1}.name = name;
-    varargout{1}.version = version;
-    varargout{1}.build_data = build_data;
-    varargout{1}.contact = contact;
-    varargout{1}.licence = licence;
+    varargout{1} = verStruct;
 end
 
 end

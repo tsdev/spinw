28f33fd2d1f885f8ac72520ecdb5103962a4f2f7
tothsa@gmail.com <tothsa@gmail.com@22f1fdcd-2ddb-f10a-0bf6-981d8240d77d>
2014-08-13 20:47:17 +0000

    Developing Monte Carlo Methods!

--- m_files/usefull/sw_sub1.m
+++ m_files/usefull/sw_sub1.m
@@ -1,4 +1,4 @@
-function out = sw_sub1(inp)
+function out = sw_sub1(inp, varargin)
 % converts symbolic variables into double by substituting 1 for every symbol
 %
 % out = SW_SUB1(inp)
@@ -12,12 +12,18 @@ function out = sw_sub1(inp)
 % out   Double type output with the same dimensions as the input.
 %
 
+if nargin == 1
+    dnum = 1;
+else
+    dnum = varargin{1};
+end
+
 if isa(inp,'sym')
     
 symVar = symvar(inp);
 
 if ~isempty(symVar)
-    out = double(subs(inp,symVar,ones(1,numel(symVar))));
+    out = double(subs(inp,symVar,dnum*ones(1,numel(symVar))));
 else
     out = double(inp);
 end

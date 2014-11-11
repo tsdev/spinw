017031fa01a2cb977301838acbbf131137b9f84a
tothsa@gmail.com <tothsa@gmail.com@22f1fdcd-2ddb-f10a-0bf6-981d8240d77d>
2014-11-11 21:20:51 +0000

    Symbolic calculations test phase ...

--- trunk/external/fprintf0.m
+++ trunk/external/fprintf0.m
@@ -2,7 +2,7 @@ function fprintf0(fid,varargin)
 % same as fprintf, expect if fid is zero, will not produce output.
 
 if fid~=0
-    fprintf(fid, varargin{:});
+    fprintf(fid,varargin{:});
 end
 
 end

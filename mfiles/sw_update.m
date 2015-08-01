c472775bc7e269c5c35697f49bd2170852c34474
tothsa <tothsa@wifivisscien173.ill.fr>
2015-08-01 18:01:36 +0200

    making sw_update more user friendly

--- mfiles/sw_update.m
+++ mfiles/sw_update.m
@@ -30,7 +30,9 @@ baseUrl = 'https://docs.google.com/uc?export=download&id=0BzFs7CQXhehSRXpjT0dndD
 
 if nargout == 0
     if ~isfield(swVer,'Version')
-        answer = getinput('This is a not yet released version of SpinW, update is not recommended! Do you want to continue? (y/n)','yn');
+        answer = getinput(...
+            ['This is a not yet released version of SpinW, update is not recommended!\n'...
+            'Do you want to continue? (y/n)'],'yn');
         
         if answer == 'n'
             disp('SpinW update process cancelled!');
@@ -126,21 +128,15 @@ zipList = unzip([installDir updateName],installDir);
 folName = [installDir strtok(zipList{1}(numel(installDir)+1:end),filesep)];
 
 % remove old SpinW installation from path
-disp('Removing path to old SpinW installation!')
+fprintf('\nRemoving path to old SpinW installation!\n')
 rmpath(genpath(sw_rootdir));
 
 % adding new path
 fprintf('Adding path to new SpinW installation: %s!\n',folName);
+ww = warning;
+warning('off'); %#ok<WNOFF>
 addpath(genpath(folName));
-
-if numel(newMsg)>0
-    disp('Release information:')
-    disp(repmat('-',[1 60]))
-    for ii = 1:numel(newMsg)
-        fprintf('\t%s\n',newMsg{ii});
-    end
-    disp(repmat('-',[1 60]))
-end
+warning(ww);
 
 disp('Removing unnecessary files... ')
 delete([installDir updateName]);
@@ -158,6 +154,14 @@ if ~verLessThan('matlab', '8.1')
     for ii = 1:numel(fList)
         delete([folName filesep 'external' filesep fList(ii).name]);
     end
+else
+    % rename the functions to be used in Matlab versions prior to R2014a
+    % strjoin()
+    movefile([folName filesep 'external' filesep 'strjoin0.m'],...
+        [folName filesep 'external' filesep 'strjoin.m']);
+    % strsplit()
+    movefile([folName filesep 'external' filesep 'strsplit0.m'],...
+        [folName filesep 'external' filesep 'strsplit.m']);
 end
 
 % functions introduced in R2015a
@@ -169,9 +173,66 @@ end
 %     end
 % end
 
-disp('In oder to load the new class definitions, issue a ''clear classes'' command before using SpinW!');
-disp('In order to reach SpinW after every restart of Matlab, add the following line to your startup.m file:');
-fprintf('addpath(genpath(''%s''));\n',folName);
+fprintf(['In order to reach SpinW after restarting Matlab, the following\n'...
+    'line has to be added to your startup.m file:\n']);
+fprintf('  addpath(genpath(''%s''));\n',folName);
+
+% location of Matlab startup file
+sfLoc = which('startup');
+uPath = userpath;
+% remove ':' and ';' characters from the userpath
+uPath = [uPath(~ismember(uPath,':;')) filesep 'startup.m'];
+
+% create new startup.m file
+if isempty(sfLoc)
+    answer = getinput(sprintf(['You don''t have a Matlab startup.m file,\n'...
+        'do you want it to be created at %s? (y/n)'],uPath),'yn');
+    if answer == 'y'
+        fclose(fopen(uPath,'w'));
+        sfLoc = uPath;
+    end
+end
+
+if ~isempty(sfLoc)
+    
+    answer = getinput(sprintf(['Would you like to add the following line:\n'...
+        sprintf('addpath(genpath(''%s''));',folName) '\nto the end of '...
+        'your Matlab startup file (%s)? (y/n)'],sfLoc),'yn');
+    
+    if answer == 'y'
+        fid = fopen(sfLoc,'a');
+        fprintf(fid,['\n%%###SW_UPDATE\n%% Path to the SpinW (rev. %d) '...
+            'toolbox:\naddpath(genpath(''%s''));\n%%###SW_UPDATE\n'],newRev,folName);
+        fclose(fid);
+    end
+end
+
+if numel(newMsg)>0
+    answer = getinput('Do you want to see the release information? (y/n)','yn');
+    if answer == 'y'
+        disp('Release information:')
+        disp(repmat('-',[1 60]))
+        for ii = 1:numel(newMsg)
+            fprintf('\t%s\n',newMsg{ii});
+        end
+        disp(repmat('-',[1 60]))
+    end
+end
+
+answer = getinput(...
+   ['\nIn order to refresh the internal class definitions of Matlab (to\n'...
+    'access the new SpinW version), issuing the "clear classes" command\n'...
+    'is necessary. However this command will also clear all your variables\n'...
+    'in the Matlab internal memory. Would you like the updater to issue\n'...
+    'the command now, otherwise you can do it manually later.\n'...
+    'Do you want to issue the command "clear classes" now? (y/n)'],'yn');
+
+if answer == 'y'
+    clear('classes'); %#ok<CLCLS>
+    disp('Matlab class memory is refreshed!')
+end
+
+
 disp('Update was successful!')
 
 end

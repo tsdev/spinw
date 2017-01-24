f51f6eba5739e8905d7c1e2e2207ba1e3bf17329
Sandor Toth <tothsa@gmail.com>
2017-01-24 11:57:30 +0100

    adding chemical bond plot

--- mfiles/+swplot/plotchem.m
+++ mfiles/+swplot/plotchem.m
@@ -94,10 +94,10 @@ inpForm.defval = [inpForm.defval {[]       []    'auto'   true      0.03     }];
 inpForm.size   = [inpForm.size   {[1 1]    [1 1] [1 -9]  [1 1]      [1 1]    }];
 inpForm.soft   = [inpForm.soft   {true     true  false   false      false    }];
 
-inpForm.fname  = [inpForm.fname  {'shift' 'replace' 'translate' 'zoom'}];
-inpForm.defval = [inpForm.defval {[0;0;0] true      true         true }];
-inpForm.size   = [inpForm.size   {[3 1]   [1 1]     [1 1]        [1 1]}];
-inpForm.soft   = [inpForm.soft   {false   false     false        false}];
+inpForm.fname  = [inpForm.fname  {'shift' 'replace' 'translate' 'zoom' 'extend' }];
+inpForm.defval = [inpForm.defval {[0;0;0] true      true         true   2       }];
+inpForm.size   = [inpForm.size   {[3 1]   [1 1]     [1 1]        [1 1]  [1 1]   }];
+inpForm.soft   = [inpForm.soft   {false   false     false        false  false   }];
 
 param = sw_readparam(inpForm, varargin{:});
 
@@ -106,6 +106,12 @@ if isempty(param.figure)
 else
     hFigure = param.figure;
 end
+
+% take care of output
+if nargout > 0
+    varargout{1} = hFigure;
+end
+
 
 if isempty(param.obj) && ~isappdata(hFigure,'obj')
     warning('plotchem:WrongInput','No SpinW object to plot!');
@@ -152,45 +158,67 @@ end
 % atom data
 atom  = obj.atom;
 
+% convert atom label to index
+if ischar(param.atom1)
+    atom1 = find(cellfun(@isempty,strfind(obj.unit_cell.label,param.atom1))==false);
+else
+    atom1 = param.atom1;
+end
+
+if ischar(param.atom2)
+    atom2 = find(cellfun(@isempty,strfind(obj.unit_cell.label,param.atom2))==false);
+else
+    atom2 = param.atom2;
+end
+
+if isempty(atom1) || isempty(atom2)
+    warning('plotchem:EmptyPlot','There are no atoms in the plotting range!')
+    return
+end
+
+% find the indices of atom1 and atom2 in the atom list
+atom1idx = find(ismember(atom.idx,atom1));
+atom2idx = find(ismember(atom.idx,atom2));
+
+
 % select atom type to plot
 switch param.mode
-    case 'all'
-        % do nothing
-    case 'mag'
-        atom.r   = atom.r(:,atom.mag);
-        atom.idx = atom.idx(1,atom.mag);
-        atom.mag = atom.mag(1,atom.mag);
-    case 'nonmag'
-        atom.r   = atom.r(:,~atom.mag);
-        atom.idx = atom.idx(1,~atom.mag);
-        atom.mag = atom.mag(1,~atom.mag);
+    case 'poly'
+    case 'bond'
     otherwise
         error('plotchem:WrongInput','The given mode string is invalid!');
 end
 
 nAtom = size(atom.r,2);
 
-% generate positions from rangelu the inclusive range
-pos = cell(1,3);
-[pos{:}] = ndgrid(rangelu(1,1):rangelu(1,2),rangelu(2,1):rangelu(2,2),rangelu(3,1):rangelu(3,2));
-pos = bsxfun(@plus,reshape(cat(4,pos{:}),[],3)',permute(atom.r,[1 3 2]));
+% generate atom1 positions from rangelu the inclusive range
+pos1 = cell(1,3);
+[pos1{:}] = ndgrid(rangelu(1,1):rangelu(1,2),rangelu(2,1):rangelu(2,2),rangelu(3,1):rangelu(3,2));
+pos1      = bsxfun(@plus,reshape(cat(4,pos1{:}),[],3)',permute(atom.r(:,atom1idx),[1 3 2]));
+nCell1    = size(pos1,2);
+% keep track of types in atom1 list
+atom1idx = repmat(atom1idx,[nCell1 1]);
+pos1     = reshape(pos1,3,[]);
 
-nCell = size(pos,2);
+% generate atom1 positions from rangelu the inclusive range
+pos2 = cell(1,3);
+% extend range for second atom by 2 unit cell in all directions
+rangeluext = [rangelu(:,1)-param.extend rangelu(:,2)+param.extend];
+[pos2{:}]  = ndgrid(rangeluext(1,1):rangeluext(1,2),rangeluext(2,1):rangeluext(2,2),rangeluext(3,1):rangeluext(3,2));
+pos2       = bsxfun(@plus,reshape(cat(4,pos2{:}),[],3)',permute(atom.r(:,atom2idx),[1 3 2]));
+nCell2     = size(pos2,2);
+% keep track of types in atom1 list
+atom2idx  = repmat(atom2idx,[nCell2 1]);
+pos2      = reshape(pos2,3,[]);
 
-% keep track of types of atoms
-aIdx = repmat(atom.idx,[nCell 1]);
-
-pos  = reshape(pos,3,[]);
-%aIdx = reshape(aIdx,3,[]);
-
-% cut out the atoms that are out of range
+% cut out the atom1 positions that are out of range
 switch param.unit
     case 'lu'
         % L>= lower range, L<= upper range
-        pIdx = all(bsxfun(@ge,pos,range(:,1)) & bsxfun(@le,pos,range(:,2)),1);
+        pIdx = all(bsxfun(@ge,pos1,range(:,1)) & bsxfun(@le,pos1,range(:,2)),1);
     case 'xyz'
         % convert to xyz
-        posxyz = BV*pos;
+        posxyz = BV*pos1;
         pIdx = all(bsxfun(@ge,posxyz,range(:,1)) & bsxfun(@le,posxyz,range(:,2)),1);
 end
 
@@ -199,98 +227,147 @@ if ~any(pIdx)
     return
 end
 
-pos  = pos(:,pIdx);
-aIdx = aIdx(pIdx);
-aIdx = aIdx(:)';
+pos1     = pos1(:,pIdx);
+atom1idx = atom1idx(pIdx);
+atom1idx = atom1idx(:)';
+atom2idx = atom2idx(:)';
 
-% color
-if strcmp(param.color,'auto')
-    color = double(obj.unit_cell.color(:,aIdx));
-else
-    color = swplot.color(param.color);
+% atom position matrices
+pos1 = repmat(permute(pos1,[2 3 1]),[1 numel(atom2idx)]);
+pos2 = repmat(permute(pos2,[3 2 1]),[numel(atom1idx) 1]);
+% atom index matrices
+atom1idx = repmat(atom1idx',[1 numel(atom2idx)]);
+atom2idx = repmat(atom2idx ,[size(atom1idx,1) 1]);
+% calculate bond vectors in lu
+dist = bsxfun(@minus,pos1,pos2);
+% convert to xyz and calculate length
+dist = sqrt(sum(sum(bsxfun(@times,permute(BV,[3 4 1 2]),dist),4).^2,3));
+% sort bond length
+[dist,sIdx] = sort(dist,2);
+% identify unique atom1
+sortidx1 = repmat((1:size(atom1idx,1))',[1 size(atom1idx,2)]);
+
+% add extra line for fake atoms
+
+
+% find bonds that fulfill the given limits
+limit = param.limit;
+if numel(limit) == 1
+    % fixed number of bonds
+    if (limit-round(limit))~=0
+        error('plotchem:WrongInput','If limit option is a single number, it has to be integer!')
+    end
+    if limit>size(sIdx,2)
+        error('plotchem:WrongInput','Increase the extend option (2) to find all furher neighbors!')
+    end
+    % convert into linear index from sort
+    sortidx1 = sortidx1(:,1:limit);
+    sIdx     = sIdx(:,1:limit);
+    
+    linIdx   = sub2ind(size(dist),sortidx1,sIdx);
+    % select the atoms that fulfill the limits
+    atom1idx = atom1idx(linIdx);
+    atom2idx = atom2idx(linIdx);
+    % also select the position vectors
+    pos1 = cat(3,pos1(linIdx),pos1(linIdx+numel(dist)),pos1(linIdx+2*numel(dist)));
+    pos2 = cat(3,pos2(linIdx),pos2(linIdx+numel(dist)),pos2(linIdx+2*numel(dist)));
+
+elseif numel(limit)==2
+    nBond1 = sum(dist>=limit(1) & dist<=limit(2),2);
+    
+    if any(diff(nBond1))
+        % polyhedra with different number of vertices
+        % loop over all atoms
+    else
+        % all polyhedra have the same number of vertices
+        sIdx = sIdx';
+        sortidx1 = sortidx1';
+        idx0 = (dist')>=limit(1) & (dist')<=limit(2);
+        sIdx = reshape(sIdx(idx0),nBond1(1),[])';
+        sortidx1 = reshape(sortidx1(idx0),nBond1(1),[])';
+    end
+    
+    linIdx   = sub2ind(size(dist),sortidx1,sIdx);
+    % select the atoms that fulfill the limits
+    atom1idx = atom1idx(linIdx);
+    atom2idx = atom2idx(linIdx);
+    % also select the position vectors
+    pos1 = cat(3,pos1(linIdx),pos1(linIdx+numel(dist)),pos1(linIdx+2*numel(dist)));
+    pos2 = cat(3,pos2(linIdx),pos2(linIdx+numel(dist)),pos2(linIdx+2*numel(dist)));
 end
-
-% radius
-switch param.radius
-    case 'auto'
-        radius = sw_atomdata(obj.unit_cell.label,'radius');
-        radius = radius(aIdx)*param.radius0;
-    case 'fix'
-        radius = param.radius0;
-    otherwise
-        error('plotchem:WrongInput','The given radius option is invalid!');
-end
-
-% prepare labels
-atom.name = obj.unit_cell.label(atom.idx);
-% plot only the first word of every label
-for ii = 1:nAtom
-    labelTemp = strword(atom.name{ii},[1 2],true);
-    label1 = labelTemp{1};
-    %label2 = labelTemp{2};
-    atom.text{ii}  = [label1 '(' num2str(atom.idx(ii)) ')_' num2str(ii)];
-    %atom.ttip{ii}  = [label2 ' atom (' label1 ')' newline 'Unit cell:' newline];
-end
-
-% save atom coordinates into data
-posDat = mat2cell(pos,3,ones(1,size(pos,2)));
 
 % shift positions
-pos = bsxfun(@plus,pos,param.shift);
+pos1 = bsxfun(@plus,pos1,permute(param.shift,[2 3 1]));
+pos2 = bsxfun(@plus,pos2,permute(param.shift,[2 3 1]));
 
-% plot label on atoms
-if param.label
-    dtext = inv(BV)*repmat(radius+param.dtext,[3 1]); %#ok<MINV>
-    text = repmat(atom.text,[nCell 1]);
-    text = text(pIdx);
-    
-    swplot.plot('type','text','name','atom_label','position',pos+dtext,...
-        'text',text,'figure',hFigure,'legend',false,'translate',false,'zoom',false,...
-        'fontsize',param.fontsize,'tooltip',false,'replace',param.replace);
+switch param.mode
+    case {'poly' 'polyhedron'}
+        % reshape pos to [3 nObject nVertex]
+        pos = permute(pos2,[3 1 2]);
+
+        % color of the polyhedron faces
+        if strcmp(param.color,'auto')
+            color = double(obj.unit_cell.color(:,atom.idx(atom1idx(:,1))));
+        else
+            color = swplot.color(param.color);
+        end
+        
+        % plot the atoms, text generated automatically
+        swplot.plot('type','polyhedron','name','chem','position',pos,...
+            'figure',hFigure,'color',color,'text','','legend',false,'label','',...
+            'nmesh',param.nmesh,'tooltip',false,'data',{},'replace',param.replace,...
+            'translate',param.translate,'zoom',param.zoom);
+    case 'bond'
+        % reshape pos to [3 nObject nVertex]
+        pos = cat(3,reshape(permute(pos1,[3 1 2]),3,[]),reshape(permute(pos2,[3 1 2]),3,[]));
+
+        if strcmp(param.color,'auto')
+            color = double(obj.unit_cell.color(:,atom.idx(atom1idx(:))));
+        else
+            color = swplot.color(param.color);
+        end
+        
+        % plot the atoms, text generated automatically
+        swplot.plot('type','cylinder','name','chem','position',pos,...
+            'figure',hFigure,'color',color,'text','','legend',false,'label','',...
+            'nmesh',param.nmesh,'tooltip',false,'data',{},'replace',param.replace,...
+            'translate',param.translate,'zoom',param.zoom);
+    otherwise
+        error('plotchem:WrongInput','The mode string is invalid!');
 end
-
-% legend label
-lLabel = repmat(atom.name,[nCell 1]);
-lLabel = lLabel(pIdx);
-
-% legend data
-lDat = getappdata(hFigure,'legend');
-
-if param.replace
-    % remove old legend entries
-    lIdx = ~ismember(lDat.name,'atom');
-    lDat.color = lDat.color(:,lIdx);
-    lDat.type  = lDat.type(:,lIdx);
-    lDat.name  = lDat.name(:,lIdx);
-    lDat.text  = lDat.text(:,lIdx);
-    setappdata(hFigure,'legend',lDat);
-    % redraw legend
-    swplot.legend('refresh',hFigure);
-end
-
-if param.legend
-    % append color
-    lDat.color = [lDat.color double(obj.unit_cell.color)/255];
-    % append type
-    lDat.type = [lDat.type 3*ones(1,obj.natom)];
-    % append name
-    lDat.name = [lDat.name repmat({'atom'},1,obj.natom)];
-    % append text
-    lDat.text = [lDat.text obj.unit_cell.label];
-    
-    setappdata(hFigure,'legend',lDat);
-    swplot.legend('on',hFigure);
-end
-
-% plot the atoms, text generated automatically
-swplot.plot('type','ellipsoid','name','atom','position',pos,'R',radius,...
-    'figure',hFigure,'color',color,'text','','legend',false,'label',lLabel,...
-    'nmesh',param.nmesh,'tooltip',false,'data',posDat,'replace',param.replace,...
-    'translate',param.translate,'zoom',param.zoom);
-
-if nargout > 0
-    varargout{1} = hFigure;
-end
+% 
+% % legend label
+% lLabel = repmat(atom.name,[nCell 1]);
+% lLabel = lLabel(pIdx);
+% 
+% % legend data
+% lDat = getappdata(hFigure,'legend');
+% 
+% if param.replace
+%     % remove old legend entries
+%     lIdx = ~ismember(lDat.name,'atom');
+%     lDat.color = lDat.color(:,lIdx);
+%     lDat.type  = lDat.type(:,lIdx);
+%     lDat.name  = lDat.name(:,lIdx);
+%     lDat.text  = lDat.text(:,lIdx);
+%     setappdata(hFigure,'legend',lDat);
+%     % redraw legend
+%     swplot.legend('refresh',hFigure);
+% end
+% 
+% if param.legend
+%     % append color
+%     lDat.color = [lDat.color double(obj.unit_cell.color)/255];
+%     % append type
+%     lDat.type = [lDat.type 3*ones(1,obj.natom)];
+%     % append name
+%     lDat.name = [lDat.name repmat({'atom'},1,obj.natom)];
+%     % append text
+%     lDat.text = [lDat.text obj.unit_cell.label];
+%     
+%     setappdata(hFigure,'legend',lDat);
+%     swplot.legend('on',hFigure);
+% end
 
 % save range
 setappdata(hFigure,'range',struct('range',param.range,'unit',param.unit));

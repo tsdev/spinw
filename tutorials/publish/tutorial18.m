f8c43a36a70cb32f986a406a38b15ddd735eda1f
Sandor Toth <tothsa@gmail.com>
2017-02-07 14:24:06 +0100

    tutorials...

--- tutorials/publish/tutorial18.m
+++ tutorials/publish/tutorial18.m
@@ -57,6 +57,7 @@ optpar.nRun = 5;
 optpar.xmin = [    zeros(1,6), 0.5 0 0.0, 0 0];
 optpar.xmax = [2*pi*ones(1,6), 1.0 0 0.5, 0 0];
 
+hK.genmagstr('mode','random','nExt',[1 1 1])
 magoptOut = hK.optmagstr(optpar);
 
 kOpt = hK.mag_str.k;
@@ -87,19 +88,20 @@ caxis([0 20])
 % approximates the propagation vector within 0.05 rlu by giving this number
 % to the 'nExt' option in spinw.genmagstr() instead of the vector with
 % three integers. We create the magnetic superlattice and then define the
-% k-vector as zero. In this case the sw.spinwave() function will be run in
-% commensurate mode.
-
-hK.genmagstr('mode','helical','next',0.05)
-hK.mag_str.k = [0 0 0]';
-
-hkSpec = hK.spinwave({[0 0 0] [1 0 0] 50},'Hermit',false);
-hK.fileid(0)
-hkSpec = sw_neutron(hkSpec,'pol',false);
-hkSpec = sw_egrid(hkSpec,'Evect',linspace(0,5,500),'imagChk',false);
-
-figure
-sw_plotspec(hkSpec,'mode','color','axlim',[0 20],'dE',0.3);
+% k-vector as zero. In this case the spinw.spinwave function will be run in
+% commensurate mode. Carefull, this code will create a magnetic supercell
+% with 270 atoms.
+% 
+% hK.genmagstr('mode','helical','next',0.05)
+% hK.mag_str.k = [0 0 0]';
+% 
+% hkSpec = hK.spinwave({[0 0 0] [1 0 0] 50},'Hermit',false);
+% hK.fileid(0)
+% hkSpec = sw_neutron(hkSpec,'pol',false);
+% hkSpec = sw_egrid(hkSpec,'Evect',linspace(0,5,500),'imagChk',false);
+% 
+% figure
+% sw_plotspec(hkSpec,'mode','color','axlim',[0 20],'dE',0.3);
 
 %% Powder averaged spectrum
 % We calculate the powder spectrum on the smaller unit cell between Q = 0 -

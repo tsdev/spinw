de5c19a263e303f4af89d7318fb81084eebff2c3
Simon Ward <substance33@gmail.com>
2017-10-24 09:57:39 +0200

    Auto stash before merge of "multiprocessor" and "tsdev/multiprocessor"

--- swfiles/@spinwR/spinwR.m
+++ swfiles/@spinwR/spinwR.m
@@ -257,14 +257,21 @@ classdef spinwR < handle
                 obj = obj.getToken();
             end
             url = strcat(obj.baseURL,'/spinw/powspec');
-            sw_opt.Q = hkl;
-            if ~isempty(varargin)
-                if ~mod(length(varargin),2)
-                    for i = 1:2:length(varargin)
-                        sw_opt.(varargin{i}) = varargin{i+1};
+            if (obj.version.Deployed)
+                sw_opt = struct();
+                sw_opt.fun = 'powspec';
+                sw_opt.argin = {obj.spinw_obj,hkl,varargin{:}};
+                sw_opt.nargout = 1;
+            else
+                sw_opt.Q = hkl;
+                if ~isempty(varargin)
+                    if ~mod(length(varargin),2)
+                        for i = 1:2:length(varargin)
+                            sw_opt.(varargin{i}) = varargin{i+1};
+                        end
+                    else
+                        error('Uneven parameter/value pairs')
                     end
-                else
-                    error('Uneven parameter/value pairs')
                 end
             end
             filename = strcat(tempname,'.mat');
@@ -344,6 +351,47 @@ classdef spinwR < handle
                 load(file,'-mat','spectra')
             end
         end
+        
+        function evaluate(obj,fn_name,varargin)
+            if isempty(obj.token)
+                error('You need to login')
+            end
+            if (obj.token_expire - datetime('now')) < 0
+                obj = obj.getToken();
+            end
+            url = strcat(obj.baseURL,'/spinw/compute');
+            if (obj.version.Deployed)
+                sw_opt = struct();
+                sw_opt.fun = fn_name;
+                sw_opt.argin = {obj.spinw_obj,varargin{:}};
+                sw_opt.nargout = 1;
+            else
+                error('This command only works on a compiled server.')
+            end
+            filename = strcat(tempname,'.mat');
+            d = char(getByteStreamFromArray(sw_opt));
+            
+            [~,remoteFName, remoteExt] = fileparts(filename);
+            opt = weboptions('Username',obj.token,'Password','x',...
+                'characterEncoding','ISO-8859-1',...
+                'MediaType','application/octet-stream',...
+                'RequestMethod','post',...
+                'HeaderFields',string({'Content-Length',string(length(d))}),...
+                'ContentType','json');
+            try
+                tempOutput = webwrite(sprintf(strcat(url,'/%s%s'),remoteFName,remoteExt), d, opt);
+            catch someException
+                throw(addCause(MException('uploadToSpinW:unableToUploadFile','Unable to upload file.'),someException));
+            end
+            if tempOutput.Calculating && ~tempOutput.Errors
+                iscomp = webread(obj.statusURL,weboptions('Username',obj.token,'ContentType','json','timeout',100));
+                if strcmp(iscomp.status,'running')
+                    obj.status = 'Calculating';
+                    obj.isCalculating = true;
+                end
+            end
+        end
+        
         
         function [username,password]=GetAuthentication(obj)
             %GetAuthentication prompts a username and password from a user and hides the

db17d0d8791a2af2286870364247d26d62312221
Sandor Toth <tothsa@gmail.com>
2017-10-26 22:56:16 +0200

    Improved testing function

--- test/partest/measfun.m
+++ test/partest/measfun.m
@@ -1,4 +1,4 @@
-function result = measfun(fun,argin,usemex,nMemSlice,nRun)
+function result = measfun(fun,argin,usemex,nMemSlice,nRun,fName)
 % measure function execution time
 %
 % `result = measfun(fun,argin,usemex,nMeMSlice,nRun)` time the function
@@ -12,27 +12,17 @@ result = [];
 header = sprintf('%-30s | %-15s | %-10s | %-7s | %-7s | %-7s | %-7s | %-7s | %-7s | %-7s\n',...
     'Function name','Speed (px/sec)','Time (s)','nQ','nMode','nWorker','nThread','nSlice','nRun','useMex');
 
-if nargin == 0
-    % just print header
+if nargin < 2
+    % print header
     fprintf(header);
-    result = [];
-    return
 end
 
 if nargin == 1
     % read the data
     % just print the data
-    result  = fun;
-    tMeas   = result.time;
-    fun     = result.fun;
-    usemex  = result.usemex;
-    nQ      = result.nQ;
-    nWorker = result.nWorker;
-    nThread = result.nThread;
-    nMemSlice = result.nSlice;
-    pps     = result.pps;
-        
-else
+    load(fun,'result');
+    
+elseif nargin > 3
     % do the measurement
     
     nThread = getenv('OMP_NUM_THREADS');
@@ -67,7 +57,6 @@ else
         nQ = nQ*3;
     end
     
-    mexstr = {'0' '1'};
     swpref.setpref('usemex',usemex,'fid',0,'tid',0);
     
     tMeas = zeros(1,nRun);
@@ -86,31 +75,37 @@ else
         pps   = round(pps*1e3)/1e3;
     end
     nMemSlice = spec.param.nSlice;
+    
+    result.time     = tMeas;
+    result.fun      = fun;
+    result.argin    = argin;
+    result.argout   = spec;
+    result.usemex   = usemex;
+    result.nQ       = nQ;
+    result.nWorker  = nWorker;
+    result.nThread  = nThread;
+    result.nSlice   = nMemSlice;
+    result.pps      = pps;
+    result.nRun     = nRun;
+    result.nMode    = nMode;
+    % save result
+    save(fName,'result');
 end
 
-tMeasStr = err2str(mean(tMeas),std(tMeas));
-ppsStr   = err2str(mean(pps),std(pps));
-
-if nThread<1
-    nThreadStr = 'max';
-else
-    nThreadStr = num2str(nThread);
+for ii = 1:numel(result)
+    rs       = result(ii);
+    mexstr   = {'0' '1'};
+    tMeasStr = err2str(mean(rs.time),std(rs.time));
+    ppsStr   = err2str(mean(rs.pps),std(rs.pps));
+    
+    if rs.nThread<1
+        nThreadStr = 'max';
+    else
+        nThreadStr = num2str(rs.nThread);
+    end
+    
+    fprintf('%-30s | %-15s | %-10s | %-7d | %-7d | %-7d | %-7s | %-7d | %-7d | %-7s\n',func2str(rs.fun),ppsStr,tMeasStr,...
+        rs.nQ,rs.nMode,rs.nWorker,nThreadStr,rs.nSlice,rs.nRun,mexstr{rs.usemex+1});
 end
-
-fprintf('%-30s | %-15s | %-10s | %-7d | %-7d | %-7d | %-7s | %-7d | %-7d | %-7s\n',func2str(fun),ppsStr,tMeasStr,...
-    nQ,nMode,nWorker,nThreadStr,nMemSlice,nRun,mexstr{usemex+1});
-
-
-result.time     = tMeas;
-result.fun      = fun;
-result.argin    = argin;
-result.argout   = spec;
-result.usemex   = usemex;
-result.nQ       = nQ;
-result.nWorker  = nWorker;
-result.nThread  = nThread;
-result.nSlice   = nMemSlice;
-result.pps      = pps;
-
 
 end

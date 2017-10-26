5e5eaadeab3b85fb99f5d1aedb09979fcaf96460
Sándor Tóth <tothsa@gmail.com>
2017-10-26 12:20:29 +0200

    Improving parallel tests

--- test/partest/partest01.m
+++ test/partest/partest01.m
@@ -12,36 +12,26 @@ swpref.setpref('usemex',false,'tid',0,'fid',0);
 yig = yig_create;
 Q = rand(3,nMat);
 
-% check parallel pool
-pPool = gcp('nocreate');
-if isempty(pPool)
-    numWorker0 = 1;
-else
-    numWorker0 = pPool.NumWorkers;
-end
+nSlice  = 4;
+nThread = 2;
 
-if numWorker0~=numWorker
-    delete(gcp('nocreate'));
-    evalc(['parpool(' num2str(numWorker) ')']);
-end
+setenv('OMP_NUM_THREADS',num2str(nThread));
 
-
-usemex = false;
-result = measfun(@spinwavefast,{yig Q},usemex, nMat);
-usemex = false;
-result(end+1) = measfun(@spinwave_spmd,{yig Q},usemex, nMat);
-
-evalc('delete(gcp)');
 % runs without parallel pool
-usemex = true;
-result(end+1) = measfun(@spinwavefast,{yig Q},usemex, nMat);
-usemex = true;
-result(end+1) = measfun(@spinwave,{yig Q},usemex, nMat);
-usemex = false;
-result(end+1) = measfun(@spinwave,{yig Q},usemex, nMat);
-usemex = false;
-result(end+1) = measfun(@spinwavefast_duc,{yig Q},usemex, nMat);
-usemex = true;
-result(end+1) = measfun(@spinwavefast_duc,{yig Q},usemex, nMat);
+evalc('delete(gcp(''nocreate''))');
+result        = measfun(@spinwavefast_duc,  {yig Q 'optmem' nSlice},false,nMat);
+result(end+1) = measfun(@spinwavefast_duc,  {yig Q 'optmem' nSlice},true, nMat);
+result(end+1) = measfun(@spinwavefast,      {yig Q 'optmem' nSlice},false,nMat);
+result(end+1) = measfun(@spinwavefast,      {yig Q 'optmem' nSlice},true, nMat);
+result(end+1) = measfun(@spinwave,          {yig Q 'optmem' nSlice},false,nMat);
+result(end+1) = measfun(@spinwave,          {yig Q 'optmem' nSlice},true, nMat);
+
+% run with parpool
+evalc(['parpool(' num2str(numWorker) ')']);
+result(end+1) = measfun(@spinwavefast,      {yig Q 'optmem' nSlice},false,nMat);
+result(end+1) = measfun(@spinwave_spmd,     {yig Q 'optmem' nSlice},false,nMat);
+
+% stop pool
+evalc('delete(gcp(''nocreate''))');
 
 end

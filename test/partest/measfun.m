6adf0c54564c81aa1f203f97fe52daae57e512ae
Sandor Toth <tothsa@gmail.com>
2017-10-26 22:47:13 +0200

    Improved testing function

--- test/partest/measfun.m
+++ test/partest/measfun.m
@@ -1,16 +1,39 @@
 function result = measfun(fun,argin,usemex,nMemSlice,nRun)
 % measure function execution time
 %
-% result = measfun(fun,argin,usemex,nMat)
+% `result = measfun(fun,argin,usemex,nMeMSlice,nRun)` time the function
 %
+% `measfun` draws the header
+%
+% `measfun(result)` plots the result
+%
+
+result = [];
+header = sprintf('%-30s | %-15s | %-10s | %-7s | %-7s | %-7s | %-7s | %-7s | %-7s | %-7s\n',...
+    'Function name','Speed (px/sec)','Time (s)','nQ','nMode','nWorker','nThread','nSlice','nRun','useMex');
 
 if nargin == 0
     % just print header
-    fprintf('%-30s | %-15s | %-10s | %-7s | %-7s | %-7s | %-7s | %-7s | %-7s | %-7s\n',...
-        'Function name','Speed (px/sec)','Time (s)','nQ','nMode','nWorker','nThread','nSlice','nRun','useMex');
-    
+    fprintf(header);
     result = [];
+    return
+end
+
+if nargin == 1
+    % read the data
+    % just print the data
+    result  = fun;
+    tMeas   = result.time;
+    fun     = result.fun;
+    usemex  = result.usemex;
+    nQ      = result.nQ;
+    nWorker = result.nWorker;
+    nThread = result.nThread;
+    nMemSlice = result.nSlice;
+    pps     = result.pps;
+        
 else
+    % do the measurement
     
     nThread = getenv('OMP_NUM_THREADS');
     nThread = str2double(nThread);
@@ -26,25 +49,17 @@ else
         nWorker = pPool.NumWorkers;
     end
     
-    if nThread<1
-        nThreadStr = 'max';
-    else
-        nThreadStr = num2str(nThread);
-    end
-    
     if iscell(argin{2})
         argin{2} = sw_qscan(argin{2});
     end
-    nQ = size(argin{2},2);
     
+    nQ = size(argin{2},2);
     if nargin > 3
         argin = [argin(:)' {'optmem' nMemSlice}];
     end
-    
     if nargin<5
         nRun = 1;
     end
-    
     % number of spin wave modes
     nMode = argin{1}.nmagext*2;
     % number of Q points triples for incommensurate structure
@@ -52,13 +67,12 @@ else
         nQ = nQ*3;
     end
     
-    
     mexstr = {'0' '1'};
     swpref.setpref('usemex',usemex,'fid',0,'tid',0);
-    %fprintf('Calling %s()...\n',func2str(fun));
     
     tMeas = zeros(1,nRun);
     
+    % measurement...
     for ii = 1:nRun
         tStart    = tic;
         spec      = fun(argin{:});
@@ -71,28 +85,32 @@ else
         tMeas = round(tMeas*1e3)/1e3;
         pps   = round(pps*1e3)/1e3;
     end
-    
-    tMeasStr = err2str(mean(tMeas),std(tMeas));
-    ppsStr   = err2str(mean(pps),std(pps));
-    
     nMemSlice = spec.param.nSlice;
-    fprintf('%-30s | %-15s | %-10s | %-7d | %-7d | %-7d | %-7s | %-7d | %-7d | %-7s\n',func2str(fun),ppsStr,tMeasStr,...
-        nQ,nMode,nWorker,nThreadStr,nMemSlice,nRun,mexstr{usemex+1});
-    
-    
-    %fprintf('%-30s | %-10s px/sec | elapsed time %-10s nQ=%d nMode=%d nWorker=%d nThread=%-2s nMemSlice=%d nRun=%d %-5s\n',...
-    %    func2str(fun),ppsStr,[tMeasStr ' s'],nQ,nMode,nWorker,nThreadStr,nMemSlice,nRun,mexstr{usemex+1});
-    
-    result.time     = tMeas;
-    result.fun      = fun;
-    result.argin    = argin;
-    result.argout   = spec;
-    result.usemex   = usemex;
-    result.nQ       = nQ;
-    result.nWorker  = nWorker;
-    result.nThread  = nThread;
-    result.nSlice   = nMemSlice;
-    result.pps      = pps;
 end
+
+tMeasStr = err2str(mean(tMeas),std(tMeas));
+ppsStr   = err2str(mean(pps),std(pps));
+
+if nThread<1
+    nThreadStr = 'max';
+else
+    nThreadStr = num2str(nThread);
+end
+
+fprintf('%-30s | %-15s | %-10s | %-7d | %-7d | %-7d | %-7s | %-7d | %-7d | %-7s\n',func2str(fun),ppsStr,tMeasStr,...
+    nQ,nMode,nWorker,nThreadStr,nMemSlice,nRun,mexstr{usemex+1});
+
+
+result.time     = tMeas;
+result.fun      = fun;
+result.argin    = argin;
+result.argout   = spec;
+result.usemex   = usemex;
+result.nQ       = nQ;
+result.nWorker  = nWorker;
+result.nThread  = nThread;
+result.nSlice   = nMemSlice;
+result.pps      = pps;
+
 
 end

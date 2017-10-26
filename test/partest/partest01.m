cbbee8d3e7673f156ac108defb0a80b69c72c4b0
Sándor Tóth <tothsa@gmail.com>
2017-10-26 11:24:54 +0200

    Minor improvements to the tests

--- test/partest/partest01.m
+++ test/partest/partest01.m
@@ -1,4 +1,4 @@
-function spec = partest01(nMat,numWorker)
+function result = partest01(nMat,numWorker)
 
 if nargin == 0
     nMat = 1e3;
@@ -21,49 +21,34 @@ else
 end
 
 if numWorker0~=numWorker
-    delete(gcp);
-    parpool(numWorker);
+    delete(gc('nocreate'));
+    evalc(['parpool(' num2str(numWorker) ')']);
 end
 
 
 usemex = false;
-spec = {};
-spec{1} = measfun(@spinwavefast,{yig Q},usemex, nMat);
 
-
-fprintf('Calling spmd+spinwave N=%d, nomex...\n',nMat)
-tStart = tic;
-Qc = Composite();
-Ni = nMat/numWorker;
-for ii = 1:numWorker
-    Qc{ii} = Q(:,(1:Ni)+(ii-1)*Ni);
-end
-spmd
-    swpref.setpref('fid',0,'tid',0);
-    specSpmd = yig.spinwave(Qc);
-    Sab = specSpmd.Sab;
-    om  = specSpmd.omega;
-end
-spec{2}.Sab = cat(4,Sab{:});
-spec{2}.omega = cat(2,om{:});
-tMeas  = toc(tStart);
-fprintf('Elapsed %5.3 s.\n',tMeas);
-
-delete(gcp)
-usemex = true;
-spec{end+1} = measfun(@spinwavefast,{yig Q},usemex, nMat);
-usemex = true;
-spec{end+1} = measfun(@spinwave,{yig Q},usemex, nMat);
+result = measfun(@spinwavefast,{yig Q},usemex, nMat);
 usemex = false;
-spec{end+1} = measfun(@spinwave,{yig Q},usemex, nMat);
-usemex = false;
-spec{end+1} = measfun(@spinwavefast_duc,{yig Q},usemex, nMat);
+result(end+1) = measfun(@spinwave_spmd,{yig Q},usemex, nMat);
+
+evalc('delete(gcp)');
+
+% runs without parallel pool
 usemex = true;
-spec{end+1} = measfun(@spinwavefast_duc,{yig Q},usemex, nMat);
+result(end+1) = measfun(@spinwavefast,{yig Q},usemex, nMat);
+usemex = true;
+result(end+1) = measfun(@spinwave,{yig Q},usemex, nMat);
+usemex = false;
+result(end+1) = measfun(@spinwave,{yig Q},usemex, nMat);
+usemex = false;
+result(end+1) = measfun(@spinwavefast_duc,{yig Q},usemex, nMat);
+usemex = true;
+result(end+1) = measfun(@spinwavefast_duc,{yig Q},usemex, nMat);
 
 end
 
-function spec = measfun(fun,argin,usemex,nMat)
+function result = measfun(fun,argin,usemex,nMat)
 % measure function execution time
 
 % check parallel pool
@@ -81,6 +66,13 @@ tStart = tic;
 spec   = fun(argin{:});
 tMeas  = toc(tStart);
 fprintf('Elapsed time %5.3f s.\n',tMeas);
+
+result.time     = tMeas;
+result.fun      = fun;
+result.argin    = argin;
+result.usemex   = usemex;
+result.nmat     = nMat;
+result.spec     = spec;
 
 end
 

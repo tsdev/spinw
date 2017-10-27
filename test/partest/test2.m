c9d48ee4d03f4dd865098677a05ae38d30126d41
Sandor Toth <tothsa@gmail.com>
2017-10-27 23:24:39 +0200

    Improved testing function

--- test/partest/test2.m
+++ test/partest/test2.m
@@ -8,6 +8,6 @@ if nargin<2
     nQ = 1e5;
 end
 
-partest2('nQ',nQ,'nWorker',zeros(1,0),'nThread',4,'nRun',5,'fName','test01.mat','nSlice',1,'hermit',true)
+partest2('nQ',nQ,'nWorker',[8 16 24 32],'nThread',4,'nRun',5,'fName','test01.mat','nSlice',1,'hermit',true)
 
 end

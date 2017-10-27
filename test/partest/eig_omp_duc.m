81f65cfad821b05b18a142ef1b8e2d8c4c8419a2
Sandor Toth <tothsa@gmail.com>
2017-10-27 23:36:41 +0200

    Improved testing function

--- test/partest/eig_omp_duc.m
+++ test/partest/eig_omp_duc.m
@@ -10,40 +10,42 @@ useMex = swpref.getpref('usemex',[]);
 % check parallel pool
 pPool = gcp('nocreate');
 if isempty(pPool)
-    numWorker = 1;
-    nn = 0;
+    numWorker = 0;
 else
     numWorker = pPool.NumWorkers;
-    nn = numWorker;
 end
 
-M(3) = round(M(3)/numWorker)*numWorker;
-
-M = num2cell(M);
-M = rand(M{:});
-if param.hermit
-    M = (M+permute(M,[2 1 3]))/2;
+if numWorker==0
+    
+    M = num2cell(M);
+    M = rand(M{:});
+    if param.hermit
+        M = (M+permute(M,[2 1 3]))/2;
+    end
+    spec = struc('V',{},'D',{});
+    [spec.V,spec.D] = eigorth(M, tol, true, useMex);
+    
+else
+    M(3) = round(M(3)/numWorker)*numWorker;
+    
+    M = num2cell(M);
+    M = rand(M{:});
+    if param.hermit
+        M = (M+permute(M,[2 1 3]))/2;
+    end
+    
+    Mc = Composite();
+    Mi = M(3)/numWorker;
+    
+    for ii = 1:numWorker
+        Mc{ii} = M(:,:,(1:Mi)+(ii-1)*Mi);
+    end
+    spmd(nn)
+        [V,D] = eigorth(Mc, tol, true, useMex);
+    end
+    
+    spec   = struct;
+    spec.V = cat(3,V{:});
+    spec.D = cat(2,D{:});
 end
-
-% [spec.V,spec.D] = eigorth(M, tol, true, useMex);
-% 
-% spec.param.nSlice = 1;
-% spec.param.hermit = param.hermit;
-
-Mc = Composite();
-Mi = M(3)/numWorker;
-
-for ii = 1:numWorker
-    Mc{ii} = M(:,:,(1:Mi)+(ii-1)*Mi);
-end
-nn
-Mc
-spmd(nn)
-    [V,D] = eigorth(Mc, tol, true, useMex);
-end
-
-spec   = struct;
-spec.V = cat(3,V{:});
-spec.D = cat(2,D{:});
-
 end

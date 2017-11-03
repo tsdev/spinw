0e002664dcbb3e1ae16398adbba0973175a064ba
Sandor Toth <tothsa@gmail.com>
2017-11-03 20:55:16 +0100

    minor changes to spmd test

--- dev/spinw/spinwavefast_duc_spmd.m
+++ dev/spinw/spinwavefast_duc_spmd.m
@@ -10,19 +10,29 @@ end
 
 Q      = sw_qscan(hkl);
 nPoint = size(Q,2);
-nPoint = round(nPoint/numWorker)*numWorker;
+nPoint = floor(nPoint/numWorker)*numWorker;
 Q      = Q(:,1:nPoint);
 
+% number of cores
+nCore   = feature('numcores');
+% number of threads per worker
+nThread = floor(nCore*2/numWorker);
 
 Qc = Composite();
 Ni = nPoint/numWorker;
+
+% data to transfer to workers
+data = {obj varargin nThread};
 
 for ii = 1:numWorker
     Qc{ii} = Q(:,(1:Ni)+(ii-1)*Ni);
 end
 spmd
+    % set number of threads
+    warning('off','MATLAB:maxNumCompThreads:Deprecated');
+    maxNumCompThreads(data{3});
     swpref.setpref('fid',0,'tid',0);
-    spec  = spinwavefast_duc(obj,Qc,varargin{:});
+    spec  = spinwavefast_duc(data{1},Qc,data{2}{:});
     Sperp = spec.Sperp;
     om    = spec.omega;
 end

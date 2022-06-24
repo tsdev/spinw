8075d595d3a63d82b0d96fcff20d3614c02f433f
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-06-24 14:49:51 +0100

    Add initial simple whole struct test

--- +sw_tests/+unit_tests/unittest_spinwave.m
+++ +sw_tests/+unit_tests/unittest_spinwave.m
@@ -1,10 +1,30 @@
-classdef unittest_spinwave < matlab.mock.TestCase
+classdef unittest_spinwave < sw_tests.unit_tests.unittest_super
     % Runs through unit test for @spinw/spinwave.m
 
     properties
         swobj = [];
         relToll = 0.01;
         absToll = 1e-6;
+        default_spinwave = struct('formfact', boolean(0), ...
+                                  'incomm', boolean(0), ...
+                                  'helical', boolean(0), ...
+                                  'norm', boolean(0), ...
+                                  'nformula', 0, ...
+                                  'param', struct('notwin', boolean(1), ...
+                                                  'sortMode', boolean(1), ...
+                                                  'tol', 1e-4, ...
+                                                  'omega_tol', 1e-5, ...
+                                                  'hermit', boolean(1)), ...
+                                  'title', 'Numerical LSWT spectrum', ...
+                                  'gtensor', boolean(0), ...
+                                  'datestart', '', ...
+                                  'dateend', '');
+    end
+
+    properties (TestParameter)
+        % Test directions and literal qpts work
+        qpts_h5 = {{[0 0 0], [1 0 0], 5}, ...
+                   [0:0.25:1; zeros(2,5)]}
     end
 
     methods (TestClassSetup)
@@ -29,6 +49,26 @@ classdef unittest_spinwave < matlab.mock.TestCase
             testCase.assertEqual(help_function.n_calls, 1);
             testCase.assertEqual(help_function.arguments, {{'spinw.spinwave'}});
         end
+        function test_sw_output(testCase, qpts_h5)
+            sw_out = testCase.swobj.spinwave(qpts_h5);
+
+            expected_hkl = [0:0.25:1; zeros(2,5)];
+            expected_Sab = zeros(3, 3, 2, 5);
+            expected_Sab([1 9 10 18 19 27 28 36 37 45 46 ...
+                          54 55 63 64 72 73 81 82 90]) = 0.5;
+            expected_Sab([7 12 25 30 43 48 61 66 75 88]) =  0.5i;
+            expected_Sab([3 16 21 34 39 52 57 70 79 84]) = -0.5i;
+
+            expected_sw_out = testCase.default_spinwave;
+            expected_sw_out.omega = [ 1e-5  2.  4.  2. -1e-5; ...
+                                     -1e-5 -2. -4. -2.  1e-5];
+            expected_sw_out.Sab = expected_Sab;
+            expected_sw_out.hkl = expected_hkl;
+            expected_sw_out.hklA = expected_hkl*2.0943951023932;
+            expected_sw_out.obj = testCase.swobj;
+            testCase.verify_spinwave(expected_sw_out, sw_out, ...
+                                     'rel_tol', 1e-10);
+        end
         function test_symbollic(testCase)
             % Test that spinw.spinwavesym() is called if spinw.symbolic==true and spinw.spinwave() is called
             % Mock the spinwavesym and symbolic methods
@@ -46,7 +86,7 @@ classdef unittest_spinwave < matlab.mock.TestCase
         end
         function test_incommensurate(testCase)
             % Tests that incommensurate calculation is ok
-            hkl = {[0 0 0] [0 1 0] [1 0 0] 50};
+            hkl = {[0 0 0] [0 1 0] [1 0 0] 5};
             commensurate_spec = testCase.swobj.spinwave(hkl);
             testCase.swobj.genmagstr('mode', 'direct', 'k', [0.123 0 0], 'n', [1 0 0], 'S', [0; 1; 0]);
             % Forcing incomm struct means exchange parameters don't agree, so we need 'hermit' false

8fa2e9a9b89bea972e5aa697fbc8c83b19cb0848
Richard Waite <richard.waite@stfc.ac.uk>
2022-12-07 15:45:34 +0000

    Do not do hermit=1 calc for BiFeO3 supercell
    As per code comment - the supercell a slightly different structure that doesn't give
    positive-definite Hamiltonian to diagonlize so cannot solve with hermit=1

--- +sw_tests/+performance_tests/BiFeO3.m
+++ +sw_tests/+performance_tests/BiFeO3.m
@@ -6,7 +6,7 @@ function BiFeO3()
                    'sym', 'R 3 c');
     bfo.addatom('r', [0 0 0.2212], 'S', 2.5, 'label', 'MFe3');
     bfo.gencoupling('maxDistance', 20)
-    bfo.addmatrix('label', 'J1', 'value', 0.6904);
+    bfo.addmatrix('label', 'J1', 'value', 0.69029);
     bfo.addmatrix('label', 'J2', 'value', 0.2)
     bfo.addcoupling('mat', 'J1', 'bond', 1)
     bfo.addcoupling('mat', 'J2', 'bond', 2)
@@ -18,21 +18,26 @@ function BiFeO3()
     % test parameters
     % add omega tol for imag eigenvalues (ignored if hermit=0)
     spinwave_args_common = {{[-1/2 0 0], [0 0 0], [1/2 1/2 0], 30}, ...
-                             'sortMode', false, 'omega_tol', 0.08};
+                             'sortMode', false, 'hermit', 0, ...
+                             'omega_tol', 0.05};
     egrid_args = {'component','Sperp','Evect',0:0.1:5, 'imagChk', 0};
     inst_args = {'dE',0.1};
     
     % do a spin wave calculation for incommensurate case ([nExt=[1,1,1]) 
     % and commensurate nExt=0.01 - which corresponds to a supercell of 
     % [11 11 1]
-    for do_supercell = [1] % 0:1
+    for do_supercell = 0:1
         if do_supercell
             nExt = 0.01;
             bfo.genmagstr('nExt', nExt)
+            % supercell slightly different structure that doesn't give 
+            % positive-definite Hamiltonian so cannot solve with hermit=1
+            hermits = 0; 
         else
             nExt = [1,1,1];
+            hermits = 0:1;
         end
-        for hermit = 0:1
+        for hermit = hermits
             for optmem = 0:5:10
                 spinwave_args = [spinwave_args_common, ...
                              'hermit', hermit, 'optmem', optmem];

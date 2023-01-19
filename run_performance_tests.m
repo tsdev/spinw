67de88e221c28cb54e0b2d1bd88c615a8d833ab1
Richard Waite <richard.waite@stfc.ac.uk>
2023-01-19 15:06:56 +0000

    Remove git checkout from script runner

--- run_performance_tests.m
+++ run_performance_tests.m
@@ -1,4 +1,4 @@
-function run_performance_tests(commit)
+function run_performance_tests()
     disp(version);
     if ~exist('spinw', 'class')
         if exist('swfiles', 'dir') && exist('external', 'dir') && exist('dat_files', 'dir')
@@ -12,18 +12,6 @@ function run_performance_tests(commit)
         end
     end
     
-    % checkout a hard-coded commit sha for last updated benchmarking result
-    % unless a specific commit (or branch name) is provided.
-    if nargin == 0
-        commit = "b0e60e32aa3790416b65386a721682e5361c1fb3";
-    end
-    current_branch = evalc("!git rev-parse --abbrev-ref HEAD");
-    try
-        eval(sprintf("!git checkout %s", commit));
-    catch
-        error('Could not checkout the commit provided.')
-    end
-    
     % run tests
     fpath_parts = {'+sw_tests', '+performance_tests'};
     files  = dir(fullfile(fpath_parts{:}, '*.m'));
@@ -31,6 +19,4 @@ function run_performance_tests(commit)
         run(erase(strjoin([fpath_parts, fname{1}], '.'), '+'));
     end
 
-    % checkout initial branch
-    eval(sprintf("!git checkout %s", current_branch(1:end-1)))
 end

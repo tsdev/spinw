70a7aeb7d7e3ba561215ead7fcb7066ea4eb9b80
Richard Waite <richard.waite@stfc.ac.uk>
2023-01-19 16:51:39 +0000

    Write tic/toc timings to file (not stdout)

--- +sw_tests/+utilities/profile_spinwave.m
+++ +sw_tests/+utilities/profile_spinwave.m
@@ -1,6 +1,25 @@
 function profile_spinwave(test_name, sw_obj, spinwave_args, egrid_args, ...
                           inst_args, do_profile)
 
+    % generate file directory name for results
+    try
+        % get current commit is possible
+        commit = evalc("!git rev-parse --short HEAD");
+        commit = commit(1:end-1); % remove newline
+    catch
+        ver = sw_version();
+        commit = [ver.Name ver.Release];
+    end
+    host_info = [computer(), '_', version('-release')];
+    save_dir = fullfile(pwd, "profile_results", commit, ...
+        host_info, test_name);
+    mkdir(save_dir)
+    
+    % open file for tic/toc timings
+    fid = fopen(fullfile(save_dir, 'tictoc_times.txt'), 'w');
+    c = onCleanup(@()fclose(fid));  % in case of exception file will close
+    fprintf(fid, "Function\tDuration (s)\n");
+
     if nargin < 6
         do_profile = true;
         % start profiling
@@ -10,37 +29,24 @@ function profile_spinwave(test_name, sw_obj, spinwave_args, egrid_args, ...
     % use supercell k=0 structure
     start_time = tic;
     spec = sw_obj.spinwave(spinwave_args{:});
-    fprintf('Elapsed time for spinwave = %.4e seconds\n', toc(start_time))
+    fprintf(fid, 'spinwave\t%.4e\n', toc(start_time));
     if ~isempty(egrid_args)
         tic;
         spec = sw_egrid(spec, egrid_args{:});
-        fprintf('Elapsed time for sw_egrid = %.4e seconds\n', toc)
+        fprintf(fid, 'sw_egrid\t%.4e\n', toc);
         if ~isempty(inst_args)
             tic;
             sw_instrument(spec, inst_args{:});
-            fprintf('Elapsed time for sw_instrument = %.4e seconds\n', toc)
+            fprintf(fid, 'sw_instrument\t%.4e\n', toc);
         end
     end
-    fprintf('Total time elapsed = %.4e seconds\n', toc(start_time))
-   
+
     if do_profile
         % save profile results
         p = profile('info');
-
-        % generate file directory name for results
-        try
-            % get current commit is possible
-            commit = evalc("!git rev-parse --short HEAD");
-            commit = commit(1:end-1); % remove newline
-        catch
-            ver = sw_version();
-            commit = [ver.Name ver.Release];
-        end
-        host_info = [computer(), '_', version('-release')];
-        save_dir = fullfile(pwd, "profile_results", commit, ...
-            host_info, test_name);
         profsave(p, save_dir);  % will mkdir if not exist
-        sw_tests.utilities.save_profile_results_to_txt(p, save_dir)
+        % save ascii summary
+        sw_tests.utilities.save_profile_results_to_txt(p, save_dir);
         profile('off');
     end
 end

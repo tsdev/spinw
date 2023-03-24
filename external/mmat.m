bc043d9ff86003e2ab1877397ee835c63e5ac797
Duc Le <duc.le@stfc.ac.uk>
2023-03-24 14:01:47 +0000

    Fix out-of-memory error in mmat

--- external/mmat.m
+++ external/mmat.m
@@ -38,6 +38,7 @@ end
 if (nargin < 3)
     dim = [1 2];
 end
+isdefaultdim = all(dim == [1 2]);
 
 if numel(dim)~=2
     error('mmat:WrongInput','dim has to be a two element array!');
@@ -51,8 +52,45 @@ nDA = ndims(A);
 nDB = ndims(B);
 nD = max(nDA,nDB);
 
+if nD == 2 && isdefaultdim
+    C = A * B;
+    return
+end
+
 nA = [size(A),ones(1,nD-nDA)]; nA = nA(dim); 
 nB = [size(B),ones(1,nD-nDB)]; nB = nB(dim);
+
+neededMem = (numel(A)*nB(2) + numel(B)*nA(1)) * 16;
+if sw_freemem < neededMem && isdefaultdim
+    % Not enough memory to expand matrix to use bsxfun; use slow loop instead
+    szA = size(A); if numel(szA) > 3, A = reshape(A, [szA(1:2) prod(szA(3:end))]); end
+    szB = size(B); if numel(szB) > 3, B = reshape(B, [szB(1:2) prod(szB(3:end))]); end
+    if numel(szA) == 2 && numel(szB) > 2
+        output_shape = [szA(1) szB(2:end)];
+        C = zeros([szA(1) szB(2) size(B,3)]);
+        for ii = 1:size(B,3)
+            C(:,:,ii) = A * B(:,:,ii);
+        end
+    elseif numel(szA) > 2 && numel(szB) == 2
+        output_shape = [szA(1) szB(2) szA(3:end)];
+        C = zeros([szA(1) szB(2) size(A,3)]);
+        for ii = 1:size(A,3)
+            C(:,:,ii) = A(:,:,ii) * B;
+        end
+    else
+        output_shape = [szA(1) szB(2:end)];
+        if size(A, 3) ~= size(B, 3), error('Extra dimensions do not agree'); end
+        C = zeros([szA(1) szB(2) size(A,3)]);
+        for ii = 1:size(A,3)
+            C(:,:,ii) = A(:,:,ii) * B(:,:,ii);
+        end
+    end
+    szC = size(C);
+    if numel(szC) ~= numel(output_shape) || ~all(szC == output_shape)
+        C = reshape(C, output_shape);
+    end
+    return
+end
 
 % form A matrix
 % (nA1) x (nA2) x nB2

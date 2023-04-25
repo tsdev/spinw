9ae44b8e6692c8f28f883dfafb9b81bed02f4c93
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2023-04-25 15:27:42 +0200

    Adapt package init

--- python/pyspinw/__init__.py
+++ python/pyspinw/__init__.py
@@ -5,20 +5,26 @@ import os
 import json
 import libpymcr
 
-_VERSION_INFO = os.path.join(os.path.dirname(__file__), 'ctfs', 'versions')
-_VERSIONS = None
-with open(_VERSION_INFO, 'r') as f:
-    _VERSIONS = json.load(f)
+_VERSION_DIR = os.path.join(os.path.dirname(__file__), 'ctfs')
+_VERSIONS = []
+for file in os.scandir(_VERSION_DIR):
+    if file.is_file() and file.name.endswith('.ctf'):
+        _VERSIONS.append({'file':    os.path.join(_VERSION_DIR, file.name),
+                          'version': 'R' + file.name.split('.')[0].split('SpinW_')[1]
+                          })
 
 
 class Matlab(libpymcr.Matlab):
     def __init__(self, *args, **kwargs):
+        initialized = False
         for version in _VERSIONS:
-            initialized = False
             try:
-                super().__init__(os.path.join(os.path.dirname(__file__), 'ctfs', version['file']), *args, **kwargs)
+                print(f"Trying MATLAB version: {version['version']} ({version['file']}))")
+                super().__init__(version['file'], *args, **kwargs)
                 initialized = True
             except RuntimeError:
                 continue
         if not initialized:
-            raise RuntimeError(f"No MATLAB version found. Please use: [{','.join([version['version'] for version in _VERSIONS])}]\n If installed, please specify the root directory of the MATLAB installation.")
+            raise RuntimeError(
+                f"No MATLAB version found. Please use: [{', '.join([version['version'] for version in _VERSIONS])}]\n "
+                f"If installed, please specify the root directory of the MATLAB installation.")
--- python/tests/test_spinw.py
+++ python/tests/test_spinw.py
@@ -1,2 +1,7 @@
 __author__ = "github.com/wardsimon"
 __version__ = "0.1.0"
+
+from pyspinw import Matlab
+
+m = Matlab()
+print(m.spinw())
